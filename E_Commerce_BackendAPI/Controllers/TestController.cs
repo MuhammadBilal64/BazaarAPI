90ab58529e07b32ffc78b80688c5bb03b25e0e45
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-03-05 00:12:14 +0500

    feat:Swagger Documentation

---
+++ E_Commerce_BackendAPI/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_BackendAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { message = "Swagger is working!" });
+    }
+}
--- E_Commerce_BackendAPI/Program.cs
+++ E_Commerce_BackendAPI/Program.cs
@@ -3,17 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "E-Commerce API V1");
+    c.RoutePrefix = string.Empty; 
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();

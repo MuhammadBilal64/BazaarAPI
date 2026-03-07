1267fabfa67c7c060994569a2fc595248da5897b
Muhammad Bilal <155463851+MuhammadBilal64@users.noreply.github.com>
2026-03-08 01:43:14 +0500

    Add ProductController and ProductDto

---
+++ E_Commerce_BackendAPI/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using E_Commerce_BackendAPI.DAL;
+using E_Commerce_BackendAPI.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("GetAll")]
+        public async Task<IActionResult>GetProducts(int page=1 ,int pagesize=10)
+        {
+            var query = _context.Products.Include(p => p.Category).Where(p => p.IsActive);
+            var totalproducts = await query.CountAsync();
+            var products = await query.Skip((page - 1) * pagesize).Take(pagesize).Select(p => new ProductDto
+            {
+
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                Stock = p.Stock,
+                CategoryName = p.Category.Name
+            }).ToListAsync();
+            return Ok(new
+            {
+                TotalCount = totalproducts,
+                Page = page,
+                PageSize = pagesize,
+                Items = products
+
+            });
+
+
+
+        }
+
+
+
+    }
+}
